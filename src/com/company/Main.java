033752beed9b059e32380e8424e4d574466cd068
AnderskdKnudsen <anderskdknudsen@gmail.com>
2017-10-10 11:42:51 +0200

    sec

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,44 @@
 package com.company;
 
+import java.io.FileNotFoundException;
+import java.util.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws FileNotFoundException
+    {
+        long startTimer;
+        long endTimer;
+        Reader reader = new Reader();
+        List<String> list = reader.getArray();
+
+        startTimer = System.currentTimeMillis();
+        Collections.sort(list);
+        Map<String, Integer> map = counter(list);
+        endTimer = System.currentTimeMillis();
+        System.out.println(endTimer-startTimer);
+
+        //
+
     }
+
+    public static Map<String, Integer> counter(List<String> list)
+    {
+        Map<String, Integer> map = new HashMap<>();
+
+        for(String s: list)
+        {
+            if(!map.containsKey(s))
+            {
+                map.put(s,1);
+            }
+            else
+            {
+                map.replace(s, map.get(s)+1);
+            }
+        }
+
+        return map;
+    }
+
 }
---
+++ src/com/company/Reader.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Created by Anders on 10-Oct-17.
+ */
+public class Reader
+{
+    private Scanner scanner = new Scanner(new File("AliceInWonderland.txt"));
+    private List<String> array = new ArrayList<>();
+
+    public Reader() throws FileNotFoundException
+    {
+        while(scanner.hasNext())
+        {
+            array.add(scanner.next().replaceAll("[\"*?!.,' '\\-()]", ""));
+        }
+    }
+
+    public List<String> getArray()
+    {
+        return array;
+    }
+}
